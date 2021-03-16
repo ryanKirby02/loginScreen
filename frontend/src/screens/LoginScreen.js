11452ff0f3c4a8284cc2efa04b593acd26e516b1
ryanKirby02 <kirbyryan36@gmail.com>
2021-03-16 15:22:31 -0400

    made the frontend of the login screen

--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -3,11 +3,34 @@ import Navbar from '../components/Navbar';
 
 import './LoginScreen.css';
 
+const onSubmitHandler = () => {
+  console.log('clicked');
+};
+
 const LoginScreen = () => {
   return (
-    <div>
+    <div className='login-container'>
       <Navbar />
-      <h1>Test</h1>
+      <div className='login-screen'>
+        <h1>Login</h1>
+        <div className='form-container'>
+          <form type='submit'>
+            <div className='form-item'>
+              <label>Email</label>
+              <input type='email' placeholder='Enter Password' />
+            </div>
+            <div className='form-item'>
+              <label>Password</label>
+              <input type='password' placeholder='Enter Password' />
+            </div>
+            <button className='submit-button'>Login</button>
+            <p>
+              Don't have an account? <a href='/register'>Register Here!</a>
+            </p>
+          </form>
+        </div>
+        <img src='/img/mountain.cms' alt='mountain' />
+      </div>
     </div>
   );
 };
