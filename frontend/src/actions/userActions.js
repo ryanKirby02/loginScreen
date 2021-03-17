bb2ea3bfcaf360b8f61d4dd13e20ba7edd728e44
ryanKirby02 <kirbyryan36@gmail.com>
2021-03-17 13:30:39 -0400

    added the register reducer register action and register screen

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -30,3 +30,33 @@ export const login = (email, password) => async (dispatch) => {
     });
   }
 };
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: 'USER_REGISTER_REQUEST',
+    });
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data } = await axios.post(
+      '/api/users/register',
+      { name, email, password },
+      config
+    );
+    dispatch({
+      type: 'USER_REGISTER_SUCCESS',
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: 'USER_REGISTER_FAIL',
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { register } from '../actions/userActions';
+
+import Navbar from '../components/Navbar';
+import './RegisterScreen.css';
+
+const RegisterScreen = ({ location, history }) => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, success, error } = userRegister;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/login';
+
+  useEffect(() => {
+    if (success) {
+      history.push(redirect);
+    }
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    dispatch(register(name, email, password));
+  };
+
+  return (
+    <div className='register-container'>
+      <Navbar />
+      <div className='register-screen'>
+        <h1>Register</h1>
+        <div className='form-container'>
+          <form onSubmit={onSubmitHandler}>
+            <div className='form-item'>
+              <label>Name</label>
+              <input
+                onChange={(e) => setName(e.target.value)}
+                value={name}
+                type='text'
+                placeholder='Enter Name'
+              />
+            </div>
+            <div className='form-item'>
+              <label>Email</label>
+              <input
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type='email'
+                placeholder='Enter Password'
+              />
+            </div>
+            <div className='form-item'>
+              <label>Password</label>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type='password'
+                placeholder='Enter Password'
+              />
+            </div>
+            <button type='submit' className='submit-button'>
+              Register
+            </button>
+            <p>
+              Already have an account? <a href='/login'>Login Here!</a>
+            </p>
+          </form>
+        </div>
+        <img src='/img/mountain.cms' alt='mountain' />
+      </div>
+    </div>
+  );
+};
+
+export default RegisterScreen;
