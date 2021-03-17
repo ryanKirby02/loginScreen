c31fdeb4fe217d2da8f57ddad1b8c939ab9d13ce
ryanKirby02 <kirbyryan36@gmail.com>
2021-03-16 20:25:03 -0400

    fixed bcrypt bug in userModels and added redux, userReducer, and userActions

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -31,11 +31,7 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error('User Already Exists, login instead...');
   }
-  const user = await User.create({
-    name,
-    email,
-    password,
-  });
+  const user = await User.create({ name, email, password });
   if (user) {
     res.status(201).json({
       name: user.name,
--- frontend/src/screens/LoginScreen.js
+++ frontend/src/screens/LoginScreen.js
@@ -1,14 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { login } from '../actions/userActions';
+
 import Navbar from '../components/Navbar';
 import './LoginScreen.css';
 
-const onSubmitHandler = () => {};
-
-const LoginScreen = () => {
+const LoginScreen = ({ location, history }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, loggedUser, error } = userLogin;
+
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+
+  useEffect(() => {
+    if (loggedUser) {
+      history.push(redirect);
+    }
+  });
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    dispatch(login(email, password));
+  };
 
   return (
     <div className='login-container'>
@@ -21,6 +39,7 @@ const LoginScreen = () => {
               <label>Email</label>
               <input
                 onChange={(e) => setEmail(e.target.value)}
+                value={email}
                 type='email'
                 placeholder='Enter Password'
               />
@@ -29,11 +48,14 @@ const LoginScreen = () => {
               <label>Password</label>
               <input
                 onChange={(e) => setPassword(e.target.value)}
+                value={password}
                 type='password'
                 placeholder='Enter Password'
               />
             </div>
-            <button className='submit-button'>Login</button>
+            <button type='submit' className='submit-button'>
+              Login
+            </button>
             <p>
               Don't have an account? <a href='/register'>Register Here!</a>
             </p>
